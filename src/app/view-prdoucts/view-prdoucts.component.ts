110a2680cafc072d19d2beb7deb13945aaf0e9dc
Varun-Babu <itsme.varun.26@gmail.com>
2022-12-07 07:29:25 +0530

    view

---
+++ src/app/view-prdoucts/view-prdoucts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-prdoucts',
+  templateUrl: './view-prdoucts.component.html',
+  styleUrls: ['./view-prdoucts.component.css']
+})
+export class ViewPrdouctsComponent {
+  data:any = []
+
+}
